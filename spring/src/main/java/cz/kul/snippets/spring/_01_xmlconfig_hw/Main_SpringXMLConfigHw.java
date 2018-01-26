5576ac9e9a8d2d4e779a49a7424a8d81fac6dd74
Ladislav Kulhanek <ladislav.kulhanek@crossad-ni.com>
2018-01-26 15:49:38 +0100

    Clearing of example 01.

--- spring/src/main/java/cz/kul/snippets/spring/_01_xmlconfig_hw/Main_SpringXMLConfigHw.java
+++ spring/src/main/java/cz/kul/snippets/spring/_01_xmlconfig_hw/Main_SpringXMLConfigHw.java
@@ -4,84 +4,32 @@ import static org.junit.Assert.assertEquals;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import cz.kul.snippets.spring.common.Bean1;
+import cz.kul.snippets.spring.common.Person;
+
 /**
- * Hello world Spring application.
- * 
- * It uses bean definition in xml. Creates a few beans and that's all.
- * 
- * @author Ladislav Kulhanek
+ * Hello world Spring application with xml configuration.
+ * It creates a few beans and that's all.
  */
 public class Main_SpringXMLConfigHw {
+	
+	public static void main(String[] args) {
+		createBeans();
+	}
 
-    public static void main(String[] args) {
+	public static void createBeans() {
         String configLocation = "cz/kul/snippets/spring/_01_xmlconfig_hw/spring.xml";
-
         try (ClassPathXmlApplicationContext ctx = new ClassPathXmlApplicationContext(configLocation)) {
-            
-            WeatherConnector connector = (WeatherConnector) ctx.getBean("WC");
-            assertEquals(WeatherConnector.class, connector.getClass());
+
+        	Person person = (Person) ctx.getBean("person");
+            assertEquals(Person.class, person.getClass());
     
-            PrototypeBean p1 = (PrototypeBean) ctx.getBean("PB", "foo"); // This automatically found appropriate constructor
-            assertEquals("foo", p1.getMsg());
-        }
+            Bean1 p1 = (Bean1) ctx.getBean("prototype");
+            assertEquals("foo", p1.getVal());
 
-    }
-
-    public static class WeatherConnector {
-
-        private String url;
-
-        private String login;
-
-        private String password;
-
-        public String getUrl() {
-            return url;
-        }
-
-        public void setUrl(String url) {
-            this.url = url;
-        }
-
-        public String getLogin() {
-            return login;
-        }
-
-        public void setLogin(String login) {
-            this.login = login;
-        }
-
-        public String getPassword() {
-            return password;
-        }
-
-        public void setPassword(String password) {
-            this.password = password;
-        }
-
-    }
-
-    /**
-     * Another Spring bean.
-     * 
-     * It has prototype scope, it means it is not singleton but new instance is always
-     * created.
-     * 
-     */
-    public static class PrototypeBean {
-
-        private String msg;
-
-        public PrototypeBean(String msg) {
-            this.msg = msg;
-        }
-
-        public String getMsg() {
-            return msg;
-        }
-
-        public void setMsg(String msg) {
-            this.msg = msg;
+            // This automatically found appropriate constructor and call it with given parameters
+            Bean1 p2 = (Bean1) ctx.getBean("prototype", "bar");
+            assertEquals("bar", p2.getVal());
         }
 
     }
