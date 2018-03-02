edbff84b9f392f347eb95b251f06b7b18a1c45bf
Ladislav Kulhanek <ladislav.kulhanek@crossad-ni.com>
2018-03-02 09:30:07 +0100

    big refactoring

--- javascript/src/main/webapp/04_events/script.js
+++
@@ -1,9 +0,0 @@
-/**
- * (1) Assign event to the DOM object
- */
-function foo() {
-  log.console("FOO function");
-}
-document.getElementById("b").onclick = function() {
-  foo();
-};
--- webservice/src/main/java/cz/kul/snippets/webservice/_01_hw/HumanResourceService.java
+++
@@ -1,9 +0,0 @@
-package cz.kul.snippets.webservice._01_hw;
-
-import java.util.Date;
-
-public interface HumanResourceService {
-	
-	void bookHoliday(Date startDate, Date endDate, String name);
-
-}
