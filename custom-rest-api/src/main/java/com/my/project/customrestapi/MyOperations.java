9045fa0963a28be0581ca9cd70d007de28dac513
Ladislav Kulhanek <ladislav.kulhanek@cross-ni.com>
2023-03-15 16:54:26 +0100

    Cross custom api

---
+++ custom-rest-api/src/main/java/com/my/project/customrestapi/MyOperations.java
@@ -0,0 +1,39 @@
+package com.my.project.customrestapi;
+
+import com.cross_ni.cross.app_int.NodeInterface;
+import com.cross_ni.cross.app_int.entity.NodeDto;
+import com.cross_ni.cross.app_int.entity.NodeReference;
+import com.cross_ni.cross.app_int.entity.NodeStatusDto;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.server.ResponseStatusException;
+
+import java.util.Map;
+import java.util.Optional;
+
+@RestController
+public class MyOperations {
+
+    @Autowired
+    private NodeInterface nodeInterface;
+
+    @GetMapping(value = "/node/{nodeId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public Object getNode(@PathVariable Long nodeId) {
+        Optional<NodeDto> nodeDtoOpt = nodeInterface.find(
+                new NodeReference(nodeId),
+                NodeDto.NodeFetch.FETCH_NODETYPES,
+                NodeDto.NodeFetch.FETCH_STATUS);
+        NodeDto dto = nodeDtoOpt.orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND));
+        return Map.of(
+                "id", dto.getNodeId(),
+                "name", dto.getName(),
+                "types", dto.getTypeDiscriminators(),
+                "status", dto.getNodeStatus().map(NodeStatusDto::getName).orElse("NOT_INITIALIZED"));
+    }
+
+}
