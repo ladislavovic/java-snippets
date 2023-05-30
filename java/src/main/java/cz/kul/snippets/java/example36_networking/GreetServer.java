498477bb7456f56b4fea0c1a9e6d092e6805aa2e
Ladislav Kulhanek <ladislav.kulhanek@cross-ni.com>
2023-05-30 11:33:14 +0200

    networking WIP

--- java/src/main/java/cz/kul/snippets/java/example36_networking/GreetServer.java
+++ java/src/main/java/cz/kul/snippets/java/example36_networking/GreetServer.java
@@ -9,9 +9,17 @@ public class GreetServer {
     private PrintWriter out;
     private BufferedReader in;
 
-    public void start(int port) {
+    public void start(int port) throws IOException {
+
+        // Creates a server socket, bound to the specified port.
         serverSocket = new ServerSocket(port);
+
+        // Listens for a connection to be made to this socket and accepts it.
+        // The method blocks until a connection is made.
         clientSocket = serverSocket.accept();
+
+
+        // TODO continue here
         out = new PrintWriter(clientSocket.getOutputStream(), true);
         in = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
         String greeting = in.readLine();
@@ -23,13 +31,13 @@ public class GreetServer {
         }
     }
 
-    public void stop() {
+    public void stop() throws IOException{
         in.close();
         out.close();
         clientSocket.close();
         serverSocket.close();
     }
-    public static void main(String[] args) {
+    public static void main(String[] args) throws Exception {
         GreetServer server=new GreetServer();
         server.start(6666);
     }
